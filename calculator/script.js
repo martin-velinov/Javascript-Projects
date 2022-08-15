c2b009929435e4d97173e64dc9cdfafeb48b1887
Martin <martin.velinov24@hotmail.com>
2022-08-15 19:49:32 +0200

    finished calculator

--- calculator/script.js
+++ calculator/script.js
@@ -9,6 +9,30 @@ function Reset(num = '0') {
     currentNumber = num;
 }
 
+function numberPress(num) {
+    currentNumber === '0' ? currentNumber = num : currentNumber += num;
+    updateDisplay(currentNumber);
+    operationMode = true;
+}
+function operatorPress(operator) {
+    if (!operationMode) {
+        return;
+    }
+
+    operations.push(operator);
+    numbers.push(currentNumber);
+
+    if (operator === 'equal') {
+        currentNumber = Calculate();
+        updateDisplay(currentNumber);
+        Reset(currentNumber);
+    } else {
+        currentNumber = '0';
+        updateDisplay();
+        operationMode = false;
+    }
+}
+
 function Calculate() {
     let total = 0;
 
@@ -44,3 +68,40 @@ function Operate(number1, number2, operation) {
 
 
 
+function updateDisplay(num = '0') {
+    const digitCount = num.length;
+    const displayElement = document.getElementById('display');
+
+    if (digitCount >= 11 && digitCount < 15) {
+        displayElement.style.fontSize = '20px';
+    } else if (digitCount >= 15) {
+        displayElement.style.fontSize = '15px';
+    } else {
+        displayElement.style.fontSize = '28px';
+    }
+
+    displayElement.innerText = num;
+}
+
+function handleButtonPress(btnId) {
+    if (btnId === 'dot') {
+        currentNumber = currentNumber + '.';
+        updateDisplay(currentNumber);
+    } else if (btnId === 'clear') {
+        Reset();
+        updateDisplay();
+    } else if (!isNaN(Number.parseInt(btnId))) {
+        numberPress(btnId);
+    } else {
+        operatorPress(btnId);
+    }
+}
+
+function init() {
+    Array.from(document.getElementsByClassName('btn'))
+        .forEach(btn => {
+            btn.addEventListener('click', () => handleButtonPress(btn.id))
+        });
+}
+
+window.addEventListener('DOMContentLoaded', init);
